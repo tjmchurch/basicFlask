cda5791147229c102c0da2c4fb19ec8f23b08419
Trent Church <church.41@wright.edu>
2022-03-09 23:56:33 -0600

    added endpoint

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello world'
+    
+@app.route('/endpoint')
+def endpoint():
+    return 'endpoint!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
